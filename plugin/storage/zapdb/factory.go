2d37bf86b89356447cb32f819efb318dc0873225
Edward Tsang <etsang@splunk.com>
2019-02-15 16:59:38 -0800

    skeleton files

--- plugin/storage/zapdb/factory.go
+++ plugin/storage/zapdb/factory.go
@@ -1,1 +1,65 @@
 package zapdb
+
+import (
+	"flag"
+
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+
+	"github.com/jaegertracing/jaeger/storage/dependencystore"
+	"github.com/jaegertracing/jaeger/storage/spanstore"
+	"github.com/uber/jaeger-lib/metrics"
+)
+
+// Factory implements storage.Factory for zapdb backend.
+type Factory struct {
+	Options *Options
+
+	metricsFactory metrics.Factory
+	logger         *zap.Logger
+}
+
+// NewFactory creates a new Factory.
+func NewFactory() *Factory {
+	return &Factory{}
+}
+
+// AddFlags implements plugin.Configurable
+func (f *Factory) AddFlags(flagSet *flag.FlagSet) {
+	return
+}
+
+// InitFromViper implements plugin.Configurable
+func (f *Factory) InitFromViper(v *viper.Viper) {
+	return
+}
+
+// Initialize implements storage.Factory
+func (f *Factory) Initialize(metricsFactory metrics.Factory, logger *zap.Logger) error {
+	return nil
+}
+
+// CreateSpanReader implements storage.Factory
+func (f *Factory) CreateSpanReader() (spanstore.Reader, error) {
+	return nil, nil
+}
+
+// CreateSpanWriter implements storage.Factory
+func (f *Factory) CreateSpanWriter() (spanstore.Writer, error) {
+	return nil, nil
+}
+
+// CreateDependencyReader implements storage.Factory
+func (f *Factory) CreateDependencyReader() (dependencystore.Reader, error) {
+	return nil, nil
+}
+
+// CreateArchiveSpanReader implements storage.ArchiveFactory
+func (f *Factory) CreateArchiveSpanReader() (spanstore.Reader, error) {
+	return nil, nil
+}
+
+// CreateArchiveSpanWriter implements storage.ArchiveFactory
+func (f *Factory) CreateArchiveSpanWriter() (spanstore.Writer, error) {
+	return nil, nil
+}
--- plugin/storage/zapdb/options.go
+++ plugin/storage/zapdb/options.go
@@ -1,1 +1,5 @@
 package zapdb
+
+// Options stores the configuration entries for this storage
+type Options struct {
+}
